62ad76eea21c206bceddb70a115393312a562dd3
JoseMMedranoJr <jose.m.medranojr+port@gmail.com>
2025-11-29 14:19:43 -0600

    Commit all modified and new files

--- guessing_game.py
+++ guessing_game.py
@@ -1,1 +1,30 @@
+#pcode
+
+# * 1. build class 
+# 2. make parameters: answer(int), 
+# 3. make a function called guess that pulls user input as parameter user_guess
+# 4. return function above with a string with an if / else method
+# 5. creat guessing game test file
+
+
 class GuessingGame():
+    answer = 7
+
+    def __init__(self, answer):
+        self.answer = answer
+
+    def guess(user_guess): # example of an *instance method*
+        if user_guess == answer:
+            return "Correct"
+        elif user_guess > answer:
+            return "High"
+        else:
+            return "Low"
+
+    def solved(self, item):
+        if user_guess == "Correct":
+            return True
+        else:
+            return False
+
+GuessingGame.guess(5) 
